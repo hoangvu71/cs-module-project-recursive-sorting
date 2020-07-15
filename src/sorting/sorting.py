39bcfcc248c21307911992ade5eed51c3b453e0b
hoangvu ong(devin) <hoangvu@hoangvus-MacBook-Air.local>
2020-07-14 17:32:39 -0700

    Done with merge, working on merge_sort

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,35 +3,46 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # Your code here
+    # Index for input arrays
     index = 0
+    # Index for return array
+    indexM = 0
+
+    # Since we are cutting the input array one by one 
+    # We can check while length of those arrays
     while len(arrA) > 0 and len(arrB) > 0:
+
+        # if arrayA smaller than arrayB
         if arrA[index] < arrB[index]:
-            for indexM in range(len(merged_arr)):
-                if merged_arr[indexM] == 0:
-                    merged_arr[indexM] = arrA[index]
-                    arrA = arrA[1:]                        
-                    break
-        elif arrA[index] >= arrB[index]:
-            for indexM in merged_arr:
-                if merged_arr[indexM] == 0:                        
-                    merged_arr[indexM] = arrB[index]
-                    arrB = arrB[1:]
-                    break
+            # Put that array into return array
+            merged_arr[indexM] = arrA[index]
+            indexM += 1
+            # Cut the input array
+            arrA = arrA[1:]
+
+        # elif arrayA bigger than arrayB
+        elif arrA[index] >= arrB[index]: 
+            # Put that array into return array                      
+            merged_arr[indexM] = arrB[index]
+            indexM += 1
+            # Cut the input array
+            arrB = arrB[1:]
+
+    # If one array is empty, and the other still have some arrays
+    # We do the same thing as above
     while len(arrA) > 0 or len(arrB) > 0:
         if len(arrA) > 0:
-            for indexM in range(len(merged_arr)):
-                if merged_arr[indexM] == 0:
-                    merged_arr[indexM] = arrA[index]
-                    arrA = arrA[1:]
-                    break            
+            merged_arr[indexM] = arrA[index]
+            indexM += 1
+            arrA = arrA[1:]
         elif len(arrB) > 0:
-                if merged_arr[indexM] == 0:
-                    merged_arr[indexM] = arrB[index]
-                    arrB = arrB[1:]
-                    break
+            merged_arr[indexM] = arrB[index]
+            indexM += 1
+            arrB = arrB[1:]
+
     return merged_arr
 
-print(merge([1,3,5,9,11,19],[2,4,6,7,8,12]))
+print(merge([1],[2]))
 # # TO-DO: implement the Merge Sort function below recursively
 # def merge_sort(arr):
 #     # Your code here
